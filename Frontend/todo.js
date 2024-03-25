89983c39ff3670aa732f52ef5910620c09d00a65
krjofficial <krishrjain.official@gmail.com>
2024-03-25 19:46:06 +0530

    -

---
+++ Frontend/todo.js
@@ -0,0 +1,5 @@
+// The input gets stored in these variables 
+// Get references to the form and task list container
+const taskForm = document.getElementById('task-form');
+const taskInput = document.getElementById('task-input');
+
